85519c8e2b1543295b19ae66f9cc086b90839bc7
Benoit AVERTY <benoit.averty@gmail.com>
2015-05-06 16:42:46 +0200

    Started javascript to generate reference links

--- core/js/htmlpagedocs.js
+++ core/js/htmlpagedocs.js
@@ -1,1 +1,27 @@
 /* Main JS script of HTMLpageDocs */
+
+/* Generate references */
+$(document).ready(function() {
+	var reflinks = $("a.reference");
+
+	var debug = $("#debug-box");
+
+	reflinks.each(function(index, reflink) {
+		var target_el = $($(reflink).attr("href"));
+
+		// The content of the target
+		$(reflink).children(".ref-content").append(target_el.text());
+
+		// The target title (closest previous heading)
+		$(reflink).children(".ref-title")
+
+		// The target page num
+		$(reflink).children(".ref-pagenum")
+
+		// The target heading counters
+		$(reflink).children(".ref-h2-num")
+
+		// The target heading counters
+		$(reflink).children(".ref-allnums")
+	});
+});
